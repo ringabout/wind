50569de19eefaf15e37125cdb6b2601083c054dd
flywind <43030857+xflywind@users.noreply.github.com>
2019-09-22 17:56:46 +0800

    Update ast.nim

--- src/ast.nim
+++ src/ast.nim
@@ -1,6 +1,20 @@
 import parser
+import strformat
 import types
 
 
-proc reduce(node: Node) = 
-  discard
+proc `$`*(root: Node): string = 
+  case root.kind 
+  of LeafNode:
+    result &= $root.value
+  of IndentNode:
+    result &= root.name
+  of IntNode:
+    result &= $root.intVar
+  of AssignNode:
+    result &= fmt"Assign({$root.left}, {$root.right})"
+  of ProgramNode:
+    for r in root.code:
+      result &= $r
+  else:
+    result &= root.repr
