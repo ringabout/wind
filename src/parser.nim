f1bc43d6dc4e1248cc69aa2860fc40147d8907a0
flywind <43030857+xflywind@users.noreply.github.com>
2019-09-22 17:56:24 +0800

    Update parser.nim

--- src/parser.nim
+++ src/parser.nim
@@ -3,9 +3,17 @@ import strutils
 import types
 import lexer
 
-proc factor(cur: var SinglyLinkedNode[Token]): Node
-proc term(cur: var SinglyLinkedNode[Token]): Node
+
+proc program*(cur: var SinglyLinkedNode[Token]): Node 
+proc statement*(cur: var SinglyLinkedNode[Token]): Node
 proc expression*(cur: var SinglyLinkedNode[Token]): Node
+proc equal*(cur: var SinglyLinkedNode[Token]): Node
+proc relational*(cur: var SinglyLinkedNode[Token]): Node
+proc add*(cur: var SinglyLinkedNode[Token]): Node
+proc mul*(cur: var SinglyLinkedNode[Token]): Node
+proc unary*(cur: var SinglyLinkedNode[Token]): Node
+proc primary*(cur: var SinglyLinkedNode[Token]): Node
+
 
 
 
@@ -20,37 +28,98 @@ proc consume(cur: var SinglyLinkedNode[Token], given: TokenKind): bool =
     true
 
 
-proc expression*(cur: var SinglyLinkedNode[Token]): Node= 
-  let t1 = cur.term
-
+proc program*(cur: var SinglyLinkedNode[Token]): Node =
+  let statement = cur.statement
+  var ndContainer = @[statement] 
   while not cur.isNil:
-    if cur.consume(TKAdd):
-      return Node(kind: AddNode, left: t1, right: cur.term)
-    elif cur.consume(TkMinus):
-      return Node(kind: MinusNode, left: t1, right: cur.term)
-    else:
-      return t1
-  return t1
+    if cur.consume(TkNewLine):
+      ndContainer.add(cur.statement)
+  Node(kind: ProgramNode, code: ndContainer)
 
+
+
+proc statement*(cur: var SinglyLinkedNode[Token]): Node = 
+  let expression = cur.expression
+  return Node(kind: LeafNode, value: expression)
+
+
+proc expression*(cur: var SinglyLinkedNode[Token]): Node = 
+  let assign = cur.equal
+  if not cur.isNil:
+    if cur.consume(TkAssign):
+      return Node(kind: AssignNode, left: assign, right: cur.equal)
+    else:
+      return Node(kind: LeafNode, value: assign)
+  return Node(kind: LeafNode, value: assign)
+
+
+proc equal*(cur: var SinglyLinkedNode[Token]): Node = 
+  let r1 = cur.relational
+  if not cur.isNil:
+    if cur.consume(TkEq):
+      return Node(kind: EqNode, left: r1, right: cur.relational)
+    elif cur.consume(TkNeq):
+      return Node(kind: NeqNode, left: r1, right: cur.relational)
+  return Node(kind: LeafNode, value: r1)
   
-proc term(cur: var SinglyLinkedNode[Token]): Node = 
-  let f1 = cur.factor
 
-  while not cur.isNil:
-    if cur.consume(TkMul):
-      return Node(kind: MulNode, left: f1, right: cur.factor)
-    elif cur.consume(TkDiv):
-      return Node(kind: DivNode, left: f1, right: cur.factor)
-    else:
-      return f1
-  return f1
+
+proc relational*(cur: var SinglyLinkedNode[Token]): Node =
+  let a1 = cur.add
+  if not cur.isNil:
+    if cur.consume(TkLt):
+      return Node(kind: LeNode, left: a1, right: cur.add)
+    elif cur.consume(TkGt):
+      return Node(kind: GtNode, left: a1, right: cur.add)
+    elif cur.consume(TkLe):
+      return Node(kind: LeNode, left: a1, right: cur.add)
+    elif cur.consume(TkGe):
+      return Node(kind: GeNode, left: a1, right: cur.add)
+  return Node(kind: LeafNode, value: a1)
+
+proc add*(cur: var SinglyLinkedNode[Token]): Node = 
+  let t1 = cur.mul
+  if not cur.isNil:
+    if cur.consume(TKAdd):
+      return Node(kind: AddNode, left: t1, right: cur.mul)
+    elif cur.consume(TkMinus):
+      return Node(kind: MinusNode, left: t1, right: cur.mul)
+
+  return Node(kind: LeafNode, value: t1)
  
+proc mul*(cur: var SinglyLinkedNode[Token]): Node = 
+  let f1 = cur.unary
+  if not cur.isNil:
+    if cur.consume(TkMul):
+      return Node(kind: MulNode, left: f1, right: cur.unary)
+    elif cur.consume(TkDiv):
+      return Node(kind: DivNode, left: f1, right: cur.unary)
+  return Node(kind: LeafNode, value: f1)
+ 
+proc unary*(cur: var SinglyLinkedNode[Token]): Node =
+  if not cur.isNil:
+    if cur.consume(TkAdd):
+      return Node(kind: LeafNode, value: cur.primary)
+    elif cur.consume(TkMinus):
+      var primary = cur.primary
+      case primary.kind:
+        of FloatNode:
+          primary.floatVar = 0 - primary.floatVar
+        of IntNode:
+          primary.intVar = 0 - primary.intVar
+        else:
+          return Node(kind: ErrorNode)
+      return Node(kind: LeafNode, value: primary)
+  return Node(kind: LeafNode, value: cur.primary)
 
-proc factor(cur: var SinglyLinkedNode[Token]): Node= 
+
+proc primary*(cur: var SinglyLinkedNode[Token]): Node = 
   let text = cur.value.text
   if cur.consume(TkLBrace):
     result = cur.expression
     doAssert cur.consume(TkRBrace)
+  elif cur.consume(TkIndent):
+    result = Node(kind: IndentNode, name: text)
   elif cur.consume(TkInt):
     result = Node(kind: IntNode, intVar: text.parseInt)
   elif cur.consume(TkFloat):
