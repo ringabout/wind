975a2fa13416fbe8cbdd27b98e572e416ea65a28
flywind <43030857+xflywind@users.noreply.github.com>
2019-09-23 15:36:57 +0800

    Update ast.nim

--- src/ast.nim
+++ src/ast.nim
@@ -5,16 +5,27 @@ import types
 
 proc `$`*(root: Node): string = 
   case root.kind 
-  of LeafNode:
-    result &= $root.value
   of IndentNode:
     result &= root.name
   of IntNode:
     result &= $root.intVar
   of AssignNode:
-    result &= fmt"Assign({$root.left}, {$root.right})"
+    result &= fmt"{$root.left} = {$root.right})"
+  of LetNode:
+    result &= fmt"let {root.letName} = {$root.letValue}"
+  of VarNode:
+    result &= fmt"var {root.varName} = {root.varValue}"
+  of LeNode:
+    result &= fmt"{$root.left} <= {$root.right}"
+  of GeNode:
+    result &= fmt"{$root.left} >= {$root.right}"
+  of LtNode:
+    result &= fmt"{$root.left} < {$root.right}"
+  of GtNode:
+    result &= fmt"{$root.left} > {$root.right}"
   of ProgramNode:
     for r in root.code:
       result &= $r
+      result &= "\n"
   else:
     result &= root.repr
